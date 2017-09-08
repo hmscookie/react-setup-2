6f3216e838c3a6705d6db7bc9db431ed0f7f4dc6
Sarah Karsh <savkarsh@gmail.com>
2017-09-07 20:38:48 -0400

    normalize and such

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>hello</h1>
+          <button>hello</button>
+          <ul><li>hello</li></ul>
+        </header>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
