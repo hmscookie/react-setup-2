078f8a48cecd7dd6063bf4ae7f486472fa3b016a
Sarah Karsh <savkarsh@gmail.com>
2017-11-17 16:20:24 -0500

    Small QA

--- dev/scripts/portfolioTop.js
+++ dev/scripts/portfolioTop.js
@@ -18,7 +18,7 @@ class PortfolioTop extends React.Component {
           <div className="row"> {/*top blurb*/}
             <div className="spacing-3"></div>
             <Zoom delay={300}><h2 className="heading-2"><span className="lines">My Work</span></h2></Zoom>
-            <Zoom delay={500}><p className="body">I've worked on numbers projects at work as part of the developmnet team, coding the front end of sites such as <a className="link-1" href="https://211ontario.ca/about-211-ontario/our-services-annual-report/2016-2017/" target="_blank">Ontario 211's 2017 annual report</a> and <a className="link-1" href="http://7words.biz/" target="_blank">Blueprint</a>. Projects that I completed at HackerYou can be found in my portfolio below.</p></Zoom>
+            <Zoom delay={500}><p className="body hello">I've worked on numbers projects at work as part of the developmnet team, coding the front end of sites such as <a className="link-1" href="https://211ontario.ca/about-211-ontario/our-services-annual-report/2016-2017/" target="_blank">Ontario 211's 2017 annual report</a> and <a className="link-1" href="http://7words.biz/" target="_blank">Blueprint</a>. Projects that I completed at HackerYou can be found in my portfolio below.</p></Zoom>
             <div className="spacing-3"></div>
           </div>
 
